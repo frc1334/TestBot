d7a218b55e6b0ec4b3e15b25444358f524e0c9a3
Frank Yu <frankyu1995@gmail.com>
2012-11-15 15:43:14 -0500

    i don't want to command more-,-

--- CommandBase.cpp
+++ CommandBase.cpp
@@ -9,12 +9,12 @@ CommandBase::CommandBase() : Command() {
 }
 
 // Initialize a single static instance of all of your subsystems to NULL
-RobotSubsystem* CommandBase::robotsubsystem = NULL;
+RobotSubsystem* CommandBase::robotSubsystem = NULL;
 OI* CommandBase::oi = NULL;
 
 void CommandBase::init() {
     // Create a single static instance of all of your subsystems. The following
 	// line should be repeated for each subsystem in the project.
-	robotsubsystem = new RobotSubsystem();
+	robotSubsystem = new RobotSubsystem();
     oi = new OI();
 }
