368e88a246f5a8849ae6833ccc9ef6030ec130cc
Victor <victor96tkd@live.com>
2012-11-25 16:22:35 -0500

    almost done

--- Robotmap.h
+++ Robotmap.h
@@ -2,12 +2,12 @@
 #define ROBOTMAP_H
 
 /**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
- 
++ * The RobotMap is a mapping from the ports sensors and actuators are wired into
++ * to a variable name. This provides flexibility changing wiring, makes checking
++ * the wiring easier and significantly reduces the number of magic numbers
++ * floating around.
++ */
+
 #define LEFT_MOTOR                    1
 #define RIGHT_MOTOR                   2
 
