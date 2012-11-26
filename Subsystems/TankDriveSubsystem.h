c0f5f30dd987a148879064cc7816d67356f27ce1
Jamie Sinn <jman.sinn@gmail.com>
2012-11-26 16:45:22 -0500

    Fixed errors again

--- Subsystems/TankDriveSubsystem.h
+++ Subsystems/TankDriveSubsystem.h
@@ -13,7 +13,7 @@ private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
 public:
-	ExampleSubsystem();
+	TankDriveSubsystem();
 	void InitDefaultCommand();
 };
 
