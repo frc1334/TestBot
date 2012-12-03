51d22b40de5b744d242d126e7882cc8cbf5ed061
Jamie Sinn <jman.sinn@gmail.com>
2012-12-03 16:30:31 -0500

    Committing is amazing
    You are jelly of my commits

--- OI.cpp
+++ OI.cpp
@@ -2,14 +2,7 @@
 
 OI::OI() 
 {
-	getLeftDrive()
-	{
-	    Joystick.GetY;
-	}
-	getRightDrive()
-	{
-		Joystick.GetX;
-	}
+	
 	// Process operator interface input here.
 }
     inline float OI::getLeftDrive()
