9ca8d75ce0ea52a168011b9414a891304fb7c485
Jamie Sinn <jman.sinn@gmail.com>
2012-12-20 16:02:41 -0500

    Fixed spelling errors

--- Subsystems/TankDriveSubsystem.h
+++ Subsystems/TankDriveSubsystem.h
@@ -13,7 +13,7 @@ private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
 public:
-	TankDriveSubsystem();
+	tankDriveSubsystem();
 	void InitDefaultCommand();
 };
 
