ea370b34297cb8981d42178df3de6be281025122
Taryna <taryna2012@gmail.com>
2013-11-18 16:27:04 -0500

    Removed demobot

---
+++ Nov18.cpp
@@ -0,0 +1,56 @@
+#include "WPILib.h"
+
+/**
+ * This is a demo program showing the use of the RobotBase class.
+ * The SimpleRobot class is the base of a robot application that will automatically call your
+ * Autonomous and OperatorControl methods at the right time as controlled by the switches on
+ * the driver station or the field controls.
+ */
+class RobotDemo : public SimpleRobot
+{
+	Jaguar left;
+	Jaguar right;
+	Joystick leftStick;
+	Joystick rightStick;
+
+public:
+	RobotDemo(void):
+		left(1),
+		right(2),
+		leftstick(1),
+		rightstick(2)
+	{
+		myRobot.SetExpiration(0.1);
+	}
+
+	/**
+	 * Drive left & right motors for 2 seconds then stop
+	 */
+	void Autonomous(void)
+	{
+		myRobot.SetSafetyEnabled(false);
+		myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
+		Wait(2.0); 				//    for 2 seconds
+		myRobot.Drive(0.0, 0.0); 	// stop robot
+	}
+
+	/**
+	 * Runs the motors with arcade steering.
+	 */
+	void OperatorControl(void)
+	{
+		left.Set(leftStick.GetY());
+		right.Set(rightStick.GetY());
+
+	}
+
+	/**
+	 * Runs during test mode
+	 */
+	void Test() {
+
+	}
+};
+
+START_ROBOT_CLASS(RobotDemo);
+
