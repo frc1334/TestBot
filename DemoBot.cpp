17c4221f4db6d1de97f0fcbb23a6edd44e9050c7
josephd <joe.robotics@hotmail.com>
2013-11-18 16:13:47 -0500

    Removed autonomous code and created tank drive

--- DemoBot.cpp
+++ DemoBot.cpp
@@ -5,18 +5,21 @@
  * The SimpleRobot class is the base of a robot application that will automatically call your
  * Autonomous and OperatorControl methods at the right time as controlled by the switches on
  * the driver station or the field controls.
- */ 
+ */
 class RobotDemo : public SimpleRobot
 {
-	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
+	Jaguar left;
+	Jaguar right;
+	Joystick leftStick;
+	Joystick rightStick;
 
 public:
 	RobotDemo(void):
-		myRobot(1, 2),	// these must be initialized in the same order
-		stick(1)		// as they are declared above.
+		left(1),
+		right(2),
+		leftStick(1),
+		rightStick(2)
 	{
-		myRobot.SetExpiration(0.1);
 	}
 
 	/**
@@ -24,25 +27,17 @@ public:
 	 */
 	void Autonomous(void)
 	{
-		myRobot.SetSafetyEnabled(false);
-		myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
-		Wait(2.0); 				//    for 2 seconds
-		myRobot.Drive(0.0, 0.0); 	// stop robot
 	}
 
 	/**
-	 * Runs the motors with arcade steering. 
+	 * Runs the motors with arcade steering.
 	 */
 	void OperatorControl(void)
 	{
-		myRobot.SetSafetyEnabled(true);
-		while (IsOperatorControl())
-		{
-			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
-			Wait(0.005);				// wait for a motor update time
-		}
+        left.Set(leftStick.GetY());
+        right.Set(rightStick.GetY());
 	}
-	
+
 	/**
 	 * Runs during test mode
 	 */
