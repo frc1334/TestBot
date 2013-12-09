b2c829d7796b21635233670ac6c5074ff9ab8fa7
jessica bae <jessica-_-bae@hotmail.com>
2013-12-09 16:16:32 -0500

    changed from tank drive to arcade drive
    changed from tank drive to arcade drive

--- jessica.cpp
+++ jessica.cpp
@@ -38,10 +38,13 @@ public:
 	 */
 	void OperatorControl(void)
 	{
-        left1.Set(leftStick.GetY());
-        left2.Set(leftStick.GetY());
-        right1.Set(rightStick.GetY());
-        right2.Set(rightStick.GetY());
+	    while (IsEnabled())
+	    {
+	        left1.Set(-(leftStick.GetY() - leftStick.GetY()));
+            left2.Set(-(leftStick.GetY() - leftStick.GetY()));
+            right1.Set(-(leftStick.GetY() + leftStick.GetY()));
+            right2.Set(-(leftStick.GetY() + leftStick.GetY()));
+	    }
 	}
 
 	/**
